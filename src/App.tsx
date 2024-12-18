0558e52323175b582cc0baa3811bba032a2ac070
andrey_kir <andrey.kirillov.kiev@gmail.com>
2024-12-18 22:58:24 +0200

    test publish

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        Here will be some moment-js playground. Edited 1
+        Here will be some moment-js playground. Edited 2
       </div>
     </>
   );
